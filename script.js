93d2aebaf047f7afab7bb2bf3c75a582e09a9673
ShadowByte24 <singhalgaurika@gmail.com>
2025-06-25 04:39:20 +0000

    check on no two decimals in a number and handle if first number is negative

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const add = (a,b) => a+b;
-const subtract = (a,b) => a-b;
-const multiply = (a,b) => a*b;
+const add = (a,b) => parseFloat((a+b).toFixed(3));
+const subtract = (a,b) => parseFloat((a-b).toFixed(3));
+const multiply = (a,b) => parseFloat((a*b).toFixed(3));
 const divide = (a,b) => {
     if(b==0){
        alert ("Cannot divide by zero");
@@ -31,35 +31,48 @@ function updateDisplay(){
 }
 
 let inputSequence = [];
+let justEvaluated = false;
 function pressButton(character){
+    
     let operators = ["+", "-","*","/"];
      // Check if character is an operator
     if(operators.includes(character)){
-        // Check if inputSequence already contains an operator
-        for(let op of operators){
-            if (inputSequence.includes(op)){
-                alert("Another operator is allowed only after evaluating or clearing. (i.e., calculator evaluates 2 operand at a time)");
-            }
-        }
-    }
+        justEvaluated=false;    
+        // Check if inputSequence contains operator prev one
+        let lastChar = inputSequence[inputSequence.length-1]
+        if(operators.includes(lastChar)){
+            alert("Two operators in a row not allowed");
+            return;
+        }    
+    } 
     if(character=="="){
+        justEvaluated =true;
         let result = evaluateInput();
         inputSequence = [...String(result)];
         updateDisplay();
         return;
     }
+    if(justEvaluated && /\d/.test(character)){
+        clearInput();
+       justEvaluated = false; 
+    }
 
-    if(operators.includes(character)){
-        let i= inputSequence.indexOf(character);
-        let secondHalf = inputSequence.slice(0,i);
-        if(character=="."){
+    if(character=="."){
+        let Part = getInputString();
+        if(Part.match(/([\+\-\*\/])/)){
+            let secPart = Part.match(/([^\+\-\*\/]+)$/);
+            if(secPart && secPart[0].includes(".")){
+            alert('No more than two decimals in a number');
+            return;
+            }
+        }else {
             if(inputSequence.includes(".")){
-            alert("already has a decimal point can't add another one");
+            alert('No more than two decimals in a number');
+            return;
+            }
         }
     }
-    }
 
-  
     inputSequence.push(character);
     updateDisplay();
 }
@@ -75,10 +88,20 @@ function getInputString(){
 
 function evaluateInput(){
     let expression = getInputString();
+    console.log(expression);
     let array = expression.split(/([\+\-\*\/])/);
-    let firstNumber = parseFloat(array[0]);
-    let sign = array[1];
-    let secondNumber = parseFloat(array[2]);
+    let firstNumber, sign, secondNumber;
+    if(array[0]==""){
+       firstNumber = -parseFloat(array[2]);
+       sign = array[3];
+       secondNumber = parseFloat(array[4]);
+    }
+    else{
+        firstNumber = parseFloat(array[0]);
+        sign = array[1];
+        secondNumber = parseFloat(array[2]);
+    }
+    console.log(array);
     return operate(firstNumber, sign, secondNumber);
 }
 
@@ -103,8 +126,5 @@ function setupButtonListeners(event){
         }});
     });
 };
-
-
-
 
 window.onload = setupButtonListeners;
